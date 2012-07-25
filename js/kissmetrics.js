d7e81616a9d7e209c4e4f89f37f374865f632c6b
Farhad Hedayati Fard <hf.farhad@gmail.com>
2012-07-25 17:07:04 +0430

    Initial commit

---
+++ js/kissmetrics.js
@@ -0,0 +1,9 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function() {
+  _kmq.push(['identify', Drupal.settings.kissmetrics.identity]);
+})();
