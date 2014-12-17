004addd98cc72d37198d4ee6edf5ed28ee301672
Joe Shindelar <eojthebrave@gmail.com>
2014-12-17 08:25:53 -0700

    Add a hook_kissmetrics_event_alter() and related documentation so that others can modify the event before it's recorded.

--- kissmetrics.api.php
+++ kissmetrics.api.php
@@ -26,3 +26,21 @@ function hook_kissmetrics_user_id_alter(&$id, $account) {
     $id = $user->mail;
   }
 }
+
+/**
+ * Alter the details of an event before it is recorded.
+ *
+ * @param array $event
+ *   An associative array of data that will make up the event.
+ *   id: The unique ID for the user this even will be associated with.
+ *   aliases: an array of aliases to associative with the ID for this event.
+ *   properties: an array of properties that will be set for this event.
+ * @param $account
+ *   The user account for which this event is being logged. Provided for
+ *   reference.
+ */
+function hook_kissmetrics_event_alter(&$event, $account) {
+  if ($account->is_kitten === TRUE) {
+    $event['properties']['is_kitten'] = 'yes';
+  }
+}
